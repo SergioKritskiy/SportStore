9aef31bca251f357909ce2769df2b909b9c340df
SergioKritskiy <olifirenkoS@i.ua>
2015-10-12 16:31:52 +0300

    Finished "Navigate + Cart"

---
+++ SportsStore.UnitTests/CartTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void Add_New_Lines_To_Cart()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart target = new Cart();
+            target.AddItem(p1,1);
+            target.AddItem(p2, 1);
+            CartLine[] res = target.Lines.ToArray();
+
+            Assert.AreEqual(res.Length,2);
+            Assert.AreEqual(res[0].Product, p1);
+            Assert.AreEqual(res[1].Product, p2);
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart target = new Cart();
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.Clear();
+            
+            Assert.AreEqual(target.Lines.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Calculate_Card_Total()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1",Price=100 };
+            Product p2 = new Product { ProductID = 2, Name = "P2",Price=150 };
+
+            Cart target = new Cart();
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            decimal res= target.ComputeTotalValue();
+
+            Assert.AreEqual(res, 250);
+        }
+
+    }
+}
---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        public CartController(IProductRepository repo) {
+            repository = repo;
+        }
+        public RedirectToRouteResult AddToCart(int productId,string returnUrl) {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (prod!=null){
+                GetCard().AddItem(prod, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl) {
+        
+            Product prod = repository.Products.FirstOrDefault(p=>p.ProductID==productId);
+            if (prod!=null){
+            GetCard().RemoveLine(prod);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCard() {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart==null){
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        public ViewResult Index(string returnUrl) {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCard(),
+                ReturnUrl = returnUrl
+            });
+        }
+    }
+}
